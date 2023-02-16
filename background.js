56be0d46010a08bc0652934bb2f842e7455db45a
Ben <ben@cai.com>
2023-02-16 01:07:34 -0500

    working dogs async awat

--- background.js
+++ background.js
@@ -1,33 +1,18 @@
-function main() {
-  document.body.style.backgroundColor = "red";
-  fetch("https://demon-slayer-api.onrender.com/v1/")
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      const imgDirty = json;
-      const imgClean = imgDirty.map((el) => {
-        const index = el.image.search(".png");
-        const newUrl = el.image.slice(0, index + 4);
-        return newUrl;
+async function main() {
+  document.body.style.backgroundColor = "green";
+
+  const imgs = document.querySelectorAll("img");
+
+  imgs.forEach((el) => {
+    console.log(el);
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log("doggy", json);
+        el.src = json.message;
+        el.srcset = json.message;
       });
-
-      console.log("imguls", imgClean);
-      const imgs = document.querySelectorAll("img");
-
-      function randomEl(array) {
-        const randomId = Math.floor(Math.random() * (array.length - 1 - 0 + 1) + 0);
-        return array[randomId];
-      }
-
-      imgs.forEach((el) => {
-        console.log(el);
-        const randomImgUrl = randomEl(imgClean);
-        console.log("randomimgurl", randomImgUrl);
-        el.src = randomImgUrl;
-        el.srcset = randomImgUrl;
-      });
-    });
+  });
 }
 
 chrome.action.onClicked.addListener((tab) => {
